2875c8e130f65a859042f82043794583a1647789
Bryan Grill <grillb@gmail.com>
2016-12-16 16:54:14 -0500

    initial commit

---
+++ plata/src/components/App/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Container, Header } from 'semantic-ui-react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Container>
+          <Header as="h1">Plata App!!</Header>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
