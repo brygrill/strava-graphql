58b5b71d19d7d5ae8c871ed52cef1b647943c1db
Bryan Grill <grillb@gmail.com>
2016-12-29 05:05:01 -0500

    add firebase

---
+++ plata/src/firebase/index.js
@@ -0,0 +1,7 @@
+import firebase from 'firebase';
+import { connect } from './config';
+
+firebase.initializeApp(connect);
+
+export const ref = firebase.database().ref();
+export const firebaseAuth = firebase.auth;
