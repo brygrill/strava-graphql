3ca5523e916f1f4534de155abc8d0a9e9f7e8450
Bryan Grill <grillb@gmail.com>
2017-04-13 04:52:58 -0400

    update tests

--- src/pages/__tests__/home.test.js
+++ src/pages/__tests__/home.test.js
@@ -3,20 +3,13 @@ import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import HomePage from '../home';
 
-const mockSchedule = [
-  { date: '04012017', time: '12pm', type: 'practice' },
-  { date: '04032017', time: '6pm', type: 'practice' },
-  { date: '04032017', time: '10:30am', type: 'practice' },
-  { date: '04032017', time: '6pm', type: 'practice' },
-];
-
 describe('<HomePage />', () => {
   it('Renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<HomePage schedule={mockSchedule} />, div);
+    ReactDOM.render(<HomePage />, div);
   });
   it('Matches snapshot', () => {
-    const component = renderer.create(<HomePage schedule={mockSchedule} />);
+    const component = renderer.create(<HomePage />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
