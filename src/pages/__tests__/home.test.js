aa3cb0e1d3bd65299bfbf0206c63b30e3d5a7da6
Bryan Grill <grillb@gmail.com>
2017-04-19 14:16:57 -0400

    fix test

--- src/pages/__tests__/home.test.js
+++ src/pages/__tests__/home.test.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MemoryRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import HomePage from '../home';
 
 describe('<HomePage />', () => {
   it('Renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<HomePage />, div);
+    ReactDOM.render(<MemoryRouter><HomePage /></MemoryRouter>, div);
   });
   it('Matches snapshot', () => {
-    const component = renderer.create(<HomePage />);
+    const component = renderer.create(
+      <MemoryRouter><HomePage /></MemoryRouter>,
+    );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
