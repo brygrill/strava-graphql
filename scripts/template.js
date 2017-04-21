7b9eea22b7d936b8b1ceea1570fc1b83c2b6e4b3
Bryan Grill <grillb@gmail.com>
2017-04-21 06:05:45 -0400

    add script to gen component

---
+++ scripts/template.js
@@ -0,0 +1,11 @@
+// @flow
+import React, { Component } from 'react';
+
+export default class MyComponent extends Component {
+  defaultProps: {};
+  state = {};
+  props: {};
+  render() {
+    return <div>MyComponent!</div>;
+  }
+}
