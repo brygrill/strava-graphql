d49a98468ed483aa46eb6bf2a847bbb3be2db0c8
Bryan Grill <grillb@gmail.com>
2017-05-02 22:04:13 -0400

    add firebase function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,6 @@
-var functions = require('firebase-functions');
+const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.sendWelcomeEmail = functions.auth.user().onCreate(event => {
+  console.log('User Signed Up!');
+  console.log(event);
+});
