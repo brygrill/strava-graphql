58bad096e918904c214028f1f0c242d6c8ef02ad
Bryan Grill <grillb@gmail.com>
2017-05-04 06:25:46 -0400

    add syncstate

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,17 +1,28 @@
 // @flow
 import React, { Component } from 'react';
 
-//import base from '../rebase';
+import base from '../rebase';
 
 export default class DashboardPage extends Component {
-  state = {};
+  state = {
+    userSchedule: null,
+  };
+
+  componentDidMount() {
+    const { user } = this.props.appState;
+    base.syncState(`schedules/${user.uid}`, {
+      context: this,
+      state: 'userSchedule',
+      isNullable: true,
+    });
+  }
 
   props: {
     appState: Object,
   };
 
   render() {
-    console.log(this.props.appState);
+    console.log(this.state);
     return <div>Hi</div>;
   }
 }
