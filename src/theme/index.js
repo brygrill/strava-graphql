ef57edadf0ba463296a23cf38e5881b447c8dd70
Bryan Grill <grillb@gmail.com>
2017-05-13 05:38:08 -0400

    rm import

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,15 +1,11 @@
 // @flow
 // https://material.io/color/#!/?view.left=0&view.right=0&primary.color=212121&secondary.color=039BE5
 
-import { lightBlue600 } from 'material-ui/styles/colors';
-
 export default {
   palette: {
-    primary1Color: lightBlue600,
+    primary1Color: '#039be5',
     primary2Color: '#006db3',
     primary3Color: '#63ccff',
   },
-  toolbar: {
-    backgroundColor: '#212121',
-  },
+  toolbar: {},
 };
