4def13fd5dfd461bcda4204140a311da9c6723a5
Bryan Grill <grillb@gmail.com>
2017-07-15 06:02:40 -0400

    add function to handle strava callback

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,8 +1,13 @@
 // @flow
 import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
 import fire from '../fire';
 
 import AppContainer from '../components/AppContainer';
+
+const clientID = process.env.REACT_APP_STRAVA_CLIENTID;
+const authUrl = `https://www.strava.com/oauth/authorize?client_id=${clientID}&response_type=code&redirect_uri=http://localhost:3000/dashboard&approval_prompt=force&scope=view_private`;
 
 export default class DashboardPage extends Component {
   state = {
@@ -11,6 +16,8 @@ export default class DashboardPage extends Component {
 
   props: {
     appState: Object,
+    location: Object,
+    history: Object,
   };
 
   fetchUserData = (key: string, user: string) => {
@@ -22,17 +29,31 @@ export default class DashboardPage extends Component {
     });
   };
 
+  handleStravaCallback = (search, history) => {
+    const params = new URLSearchParams(search);
+    const code = params.get('code');
+    if (code) {
+      console.log(code);
+      history.replace('/dashboard');
+    }
+  };
+
   componentDidMount() {
+    console.log('Dashboard - DCM');
     const { user } = this.props.appState;
+    //const { search } = this.props.location.search;
     this.fetchUserData('weeks', user.uid);
+    this.handleStravaCallback(this.props.location.search, this.props.history);
   }
 
   render() {
-    console.log(this.state);
+    //console.log(this.state);
+    //console.log(this.props);
     return (
       <AppContainer authed={this.props.appState.authed}>
         <div className="mdl-grid">
           dashboard
+          <RaisedButton label="Default" href={authUrl} />
         </div>
       </AppContainer>
     );
