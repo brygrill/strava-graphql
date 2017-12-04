eea2680b8243a38bdeb66b4d354dcf0c5397b357
Bryan Grill <grillb@gmail.com>
2017-12-04 06:28:37 -0500

    add deployment

--- app/src/config/index.js
+++ app/src/config/index.js
@@ -15,10 +15,18 @@ export const stravaBaseApi = 'https://www.strava.com/api/v3';
 
 // Connect with Strava OAuth url
 const clientID = process.env.REACT_APP_STRAVA_CLIENTID;
-const URI = 'http://localhost:3000/strava';
+const URI =
+  process.env.NODE_ENV === 'production'
+    ? 'https://stravadash.now.sh/strava'
+    : 'http://localhost:3000/strava';
 const prompt = 'force';
-export const stravaOAuthUrl = `https://www.strava.com/oauth/authorize?client_id=${String(clientID)}&response_type=code&redirect_uri=${URI}&approval_prompt=${prompt}&scope=view_private`;
+export const stravaOAuthUrl = `https://www.strava.com/oauth/authorize?client_id=${String(
+  clientID,
+)}&response_type=code&redirect_uri=${URI}&approval_prompt=${
+  prompt
+}&scope=view_private`;
 
 // Firebase function to gen strava access token
 // and save to user in realtime db
-export const fireFuncStrava = 'https://us-central1-velox-f43d6.cloudfunctions.net/strava/';
+export const fireFuncStrava =
+  'https://us-central1-velox-f43d6.cloudfunctions.net/strava/';
